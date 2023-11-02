8a9d864aeca4b27c83f982951c115193d78aadcd
Daniel Chacon <danf.chacon@gmail.com>
2023-11-02 10:30:22 -0400

    Add helper functions back

--- utilities/global.php
+++ utilities/global.php
@@ -1,4 +1,33 @@
 <?php
+
+// Get all files that are direct children of specified directory
+// -------------------------------------------
+function get_child_files($pathToDirectory) {
+    $childFiles = get_all_decendent_files($pathToDirectory);
+
+    foreach ($childFiles as $index => $directChild) {
+        if (is_array($directChild)) unset($childFiles[$index]);
+    }
+
+    return array_values($childFiles);
+}
+
+function get_all_decendent_files($pathToDirectory) {
+    $pathToDirectory = is_array($pathToDirectory) ? dirname($pathToDirectory[0].'/') : $pathToDirectory;
+    $allDirectChildren = scandir($pathToDirectory);
+    $childFiles = [];
+
+    foreach ($allDirectChildren as $index => $directChild) {
+        if (is_dir($pathToDirectory.$directChild) && $directChild != '.' && $directChild != '..') {
+            array_push($childFiles, get_all_decendent_files($pathToDirectory.$directChild.'/'));
+        } else if (is_file($pathToDirectory.$directChild)) {
+            array_push($childFiles, $pathToDirectory.$directChild);
+        }
+    }
+
+    return array_values($childFiles);
+}
+
 function print_object($data)
 {
     echo "<pre>";
