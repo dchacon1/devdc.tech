59d4a647412350f7bdc0ee6159a191a5c80413e1
Daniel Chacon <danf.chacon@gmail.com>
2023-11-06 15:16:54 -0500

    Add freecodecamp certifications

--- views/home.php
+++ views/home.php
@@ -99,10 +99,14 @@ function set_home_content()
                             <p>See my doodles!</p>
                         </a>
                     </li>
+                    <li>
+                        <a class="project-tile" href="https://www.freecodecamp.org/devdc">
+                            <p class="project-tile__title">FreeCodeCamp</p>
+                            <p>See my certifications!</p>
+                        </a>
+                    </li>
                 </ul>
 
-
-                <!-- <a id="profile-link" class="absolute-link" href="https://www.freecodecamp.org/devdc/" target="_blank">See more<span aria-label="(Opens in a new window)"><svg aria-hidden="true" width="10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 212.6 212.6"><path d="M18.9 18.8v174.8h174.7c.1-1.2.2-2.4.2-3.7v-58.5c0-1.6 0-3.2.4-4.7 1.1-4.2 5.4-7.1 9.6-6.8 4.5.4 8.2 3.9 8.7 8.3.1.8.1 1.7.1 2.5v70.9c0 7.3-3.6 10.9-10.9 10.9H10.8C3.6 212.6 0 209 0 201.9V10.8C0 3.6 3.6 0 10.7 0h72.2c5.1 0 8.8 3.1 9.6 7.9.7 4.4-1.9 8.9-6.2 10.2-1.7.5-3.6.6-5.3.6H23.1c-1.3.1-2.5.1-4.2.1z"/><path d="M180.3 18.8H135.5c-1.8 0-3.7-.1-5.3-.6-4.3-1.4-6.8-5.9-6.1-10.3.7-4.3 4.4-7.7 8.8-7.7 23.5-.1 47.1-.1 70.6 0 5 0 8.9 3.9 8.9 8.9.1 23.5.1 47.1 0 70.6 0 5.1-4.2 8.9-9.3 9-5.3 0-9.3-3.9-9.4-9.5-.1-14.2 0-28.4 0-42.6v-3.4c-.5-.3-.9-.7-1.4-1-.5 1-.9 2.3-1.7 3.1-23.3 23.3-46.6 46.7-69.9 69.9-1.7 1.6-3.9 3.1-6.1 3.7-4 1.1-8-.9-10-4.4-2-3.6-1.6-8 1.4-11.2 4.5-4.6 9.1-9.2 13.7-13.7l57.8-57.8c.9-.9 1.6-1.7 2.8-3z"/></svg></span></a> -->
                 <a class="absolute-link" href="/resume/">Check out my resume!</a>
             </div>
         </section>
